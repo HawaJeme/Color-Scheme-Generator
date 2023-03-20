ff1c33b02a7071909ca3c23560249066d9c3f4d0
Hawa Jeme <kuuzey46@gmail.com>
2023-03-20 16:20:53 +0300

    completing functionality

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 // fetch("https://www.thecolorapi.com/")
-const colorPicker = document.getElementById("color-Picker")
+const colorPicker = document.getElementById("color-picker")
 const getColorBtn = document.getElementById("get-color-btn")
 const colors = document.getElementById("colors")
 const footer = document.getElementById("footer")
+const selectBox = document.getElementById("select-box")
 
+function getSchemeDivs(colorsHex){
+    let schemeDivs
+    for(let i =0; i<5; i++){
+        if(colorsHex){
+            schemeDivs = `
+            <div style="background-color:${colorsHex};" class="schemeDivs"></div>`
+        }
+    }
+    if(schemeDivs !== undefined){
+        colors.innerHTML += schemeDivs
+    }
+    
+}
+getSchemeDivs()
 getColorBtn.addEventListener('click', ()=> {
-    fetch(`https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=5`)
+    colors.innerHTML = ""
+    const selectBoxVal = selectBox.value
+    const seedColor = colorPicker.value.replace(/\W/g, "")
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${selectBoxVal}&count=5`)
     .then(Response => Response.json())
     .then(data => {
+        footer.innerHTML = ""
         const colorSchemeArr = data.colors
             colorSchemeArr.map(color => {
                 const colorsHex = color.hex.value
-                // console.log(`<div>${colorsHex}</div>`)
-                footer.innerHTML += `<div>${colorsHex}</div>`
-                })
-        })
-    // console.log(colorPicker.value)
+                footer.innerHTML += `
+                <div>${colorsHex}</div>`
+                // footer.addEventListener('click', ()=>{
+                //     let copyClipBoard
+                //     copyClipBoard += navigator.clipboard.writeText(colorsHex)
+                //     console.log(copyClipBoard)
+                // })
+                getSchemeDivs(colorsHex)
+            })
+    })
 })
